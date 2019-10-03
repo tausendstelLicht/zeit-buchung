16bddcdb8b0edfc8d2f762d6127d6b34e29a99e9
Lukas-Hubert von Zukowski <tausendstel.licht@gmail.com>
2019-10-03 18:55:00 +0200

    * added new structure for a record entry

---
+++ src/Structure/RecordStructure.php
@@ -0,0 +1,114 @@
+<?php
+
+
+namespace ZeitBuchung\Structure;
+
+use DateTime;
+
+/**
+ * Class RecordStructure
+ *
+ * @package ZeitBuchung\Structure
+ */
+class RecordStructure
+{
+    /** @var DateTime */
+    private $start;
+
+    /** @var DateTime */
+    private $end;
+
+    /** @var string */
+    private $message;
+
+    /** @var int */
+    private $timeInMinutes;
+
+    /** @var float */
+    private $timeInHours;
+
+    /**
+     * @return DateTime
+     */
+    public function getStart(): DateTime
+    {
+        return $this->start;
+    }
+
+    /**
+     * @param DateTime $start
+     * @return void
+     */
+    public function setStart(DateTime $start): void
+    {
+        $this->start = $start;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getEnd(): DateTime
+    {
+        return $this->end;
+    }
+
+    /**
+     * @param DateTime $end
+     * @return void
+     */
+    public function setEnd(DateTime $end): void
+    {
+        $this->end = $end;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     * @return void
+     */
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimeInMinutes(): int
+    {
+        return $this->timeInMinutes;
+    }
+
+    /**
+     * @param int $timeInMinutes
+     * @return void
+     */
+    public function setTimeInMinutes(int $timeInMinutes): void
+    {
+        $this->timeInMinutes = $timeInMinutes;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTimeInHours(): float
+    {
+        return $this->timeInHours;
+    }
+
+    /**
+     * @param float $timeInHours
+     * @return void
+     */
+    public function setTimeInHours(float $timeInHours): void
+    {
+        $this->timeInHours = $timeInHours;
+    }
+}
