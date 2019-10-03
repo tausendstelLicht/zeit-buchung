9b0a69666036e37ac0608f2d37ececeea12b4a11
Lukas-Hubert von Zukowski <tausendstel.licht@gmail.com>
2019-10-03 20:28:22 +0200

    * adjusted commands after record structure implementation

--- src/Command/Report.php
+++ src/Command/Report.php
@@ -69,20 +69,20 @@ class Report extends Command
             switch (true) {
                 case preg_match('/^(0[1-9]|[12]\d|3[01])\.(0[1-9]|11|12)\.$/', $inputDate): // DD.MM.
                     $inputDate .= date('Y');
-                    $return = date('Ymd', strtotime($inputDate)) . '.log';
+                    $return = date('Ymd', strtotime($inputDate)) . '.json';
                     break;
                 case preg_match('/^(0[1-9]|[12]\d|3[01])\.(0[1-9]|11|12)\.\d{4}$/', $inputDate): // DD.MM.YYYY
-                    $return = date('Ymd', strtotime($inputDate)) . '.log';
+                    $return = date('Ymd', strtotime($inputDate)) . '.json';
                     break;
                 case preg_match('/^\d{2}\-(0[1-9]|11|12)\-(0[1-9]|[12]\d|3[01])$/', $inputDate): // YY-MM-DD
-                    $return = date('Ymd', strtotime($inputDate)) . '.log';
+                    $return = date('Ymd', strtotime($inputDate)) . '.json';
                     break;
                 case preg_match('/^(0[1-9]|11|12)\-(0[1-9]|[12]\d|3[01])$/', $inputDate): // MM-DD
                     $inputDate = date('Y') . '-' . $inputDate;
-                    $return = date('Ymd', strtotime($inputDate)) . '.log';
+                    $return = date('Ymd', strtotime($inputDate)) . '.json';
                     break;
                 case preg_match('/^\d{4}\-(0[1-9]|11|12)\-(0[1-9]|[12]\d|3[01])$/', $inputDate): // YYYY-MM-DD
-                    $return = date('Ymd', strtotime($inputDate)) . '.log';
+                    $return = date('Ymd', strtotime($inputDate)) . '.json';
                     break;
                 default:
                     $this->io->warning('Input date string is invalid. "' . $inputDate . '"');
