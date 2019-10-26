65a4ec8d98cfb04bf9d806b915c9ead437c374c3
Matthias Glaub <magl@magl.net>
2019-10-26 20:06:34 +0200

    cs fixes

--- src/Application/ZeitBuchung.php
+++ src/Application/ZeitBuchung.php
@@ -9,9 +9,18 @@ use ZeitBuchung\Command\Start;
 use ZeitBuchung\Command\Status;
 use ZeitBuchung\Command\Stop;
 
+/**
+ * Class ZeitBuchung
+ * @package ZeitBuchung\Application
+ */
 class ZeitBuchung extends Application
 {
-
+    /**
+     * ZeitBuchung constructor.
+     *
+     * @param string $name
+     * @param string $version
+     */
     public function __construct(string $name = 'UNKNOWN', string $version = 'UNKNOWN')
     {
         parent::__construct($name, $version);
@@ -20,5 +29,4 @@ class ZeitBuchung extends Application
         $this->add(new Report());
         $this->add(new Status());
     }
-
 }
