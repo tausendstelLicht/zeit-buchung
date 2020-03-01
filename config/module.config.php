e0c42d0d9273f0d655b6f2fc233544ab0dc5b83a
Lukas-Hubert von Zukowski <tausendstel.licht@gmail.com>
2020-03-01 14:14:10 +0100

    * configured service manager factories

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,13 @@
 <?php
 
+use ZeitBuchung\Command\Report;
+use ZeitBuchung\Command\Start;
+use ZeitBuchung\Command\Status;
+use ZeitBuchung\Command\Stop;
+use ZeitBuchung\Factory\CommandFactory;
+use ZeitBuchung\Factory\FileServiceFactory;
 use ZeitBuchung\Service\DateTimeService;
+use ZeitBuchung\Service\FileService;
 
 return [
     // user specific config
@@ -10,10 +17,14 @@ return [
     // module config
     'service_manager' => [
         'factories' => [
-            //
+            FileService::class => FileServiceFactory::class,
+            Report::class => CommandFactory::class,
+            Start::class => CommandFactory::class,
+            Status::class => CommandFactory::class,
+            Stop::class => CommandFactory::class,
         ],
         'invokables' => [
-            DateTimeService::class => DateTimeService::class
+            DateTimeService::class => DateTimeService::class,
         ],
     ],
 ];
